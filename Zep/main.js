53b6b6d045b1ddf0fb7c55d8c68eb8854714e036
yunhui21 <yunhui21@gmail.com>
2023-02-20 10:16:26 +0900

    스파르타코딩

---
+++ Zep/main.js
@@ -0,0 +1,26 @@
+// let Spartan = App.loadSpritesheet('spartan.png', 64, 96, {
+//     left: [0, 1, 2, 3],
+//     up: [0],
+//     down: [0],
+//     right: [0, 1, 2, 3]
+// }, 8);
+// 르타인 캐릭터 넣기
+
+// App.onSay.Add(function(Player, text){
+    
+//     if(text == 'speed up'){
+//         Player.moveSpeed = 400;
+//     }
+//     if(text == 'speed down'){
+//         Player.moveSpeed = 30;
+//     }
+
+//     App.showCenterLabel(message);
+
+// })
+// App.onJoinPlayer.Add(function(Player){
+
+//     // Player.Sprite = Spartan;
+//     // Player.sendUpdated();
+// })
+
