34c10337f5aa0ffb0d9e76213d507724a0f0016b
yunhui21 <yunhui21@gmail.com>
2023-02-28 15:31:30 +0900

    last_study

---
+++ web/projects/03.pedia/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.8fihl8l.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+import requests
+from bs4 import BeautifulSoup
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+        
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+    data = requests.get(url_receive,headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    ogtitle = soup.select_one('meta[property="og:title"]')['content']
+    ogimage = soup.select_one('meta[property="og:image"]')['content']
+    ogdesc = soup.select_one('meta[property="og:description"]')['content']
+
+    doc = {
+	    'title':ogtitle, 
+	    'image':ogimage, 
+	    'desc':ogdesc,
+	    'comment':comment_receive,     
+    }
+    db.movies.insert_one(doc)
+    return jsonify({'msg':'저장완료'})
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+	all_movies = list(db.movies.find({},{'_id':False}))
+	return jsonify({'result':all_movies})
+
+if __name__ == '__main__':
+	app.run('0.0.0.0', port=5000, debug=True)
