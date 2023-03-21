eceecea774e111ce7fffa053db432c5a6fbe2b2c
yunhui21 <yunhui21@gmail.com>
2023-03-21 11:22:36 +0900

    3/22

--- web/projects/04.bcket/app.py
+++ web/projects/04.bcket/app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.8fihl8l.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    bucket_receive = request.form['bucket_give']
+    doc = {
+        'bucket' : bucket_receive
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': '저장완료'})
     
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
